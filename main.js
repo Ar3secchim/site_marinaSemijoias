88c80b0c13dc97727b3932ec493218879930c8ce
unknown <renarasecchim@gmail.com>
2022-05-29 11:37:14 -0300

    build:update

---
+++ main.js
@@ -0,0 +1,13 @@
+ScrollReveal({
+  origin:'top',
+  distance: '50px',
+  duration:800,
+}).reveal(`#home,
+  #home img, 
+  #home .stats,
+  #services header,
+  #services .services-section,
+  #about header,
+  #about .content,
+  #contact header
+`)
